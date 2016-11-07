572953929c91d14fd426121d80641e7d3d45ec50
fsam95 <f.samir1995@gmail.com>
2016-11-07 17:32:47 -0500

    Added starter code, completed q1 part 1

---
+++ class.rkt
@@ -0,0 +1,58 @@
+#lang racket
+#| Assignment 2 - Classes
+
+This file contains your work for Questions 1 and 2, extending the basic
+class macro to include support for traits and some basic introspection.
+|#
+(provide class-meta class-trait)
+
+; QUESTION 1 (metaprogramming).
+(define-syntax class-meta
+  (syntax-rules ()
+    [(class <Class> 
+       (<attr> ...)
+       [(<method> <param> ...) <body>] 
+       ...)
+     (define (<Class> <attr> ...)
+       (lambda (msg) 
+         (cond [(equal? msg (id->string <attr>)) <attr>]
+               ...
+               [(equal? msg (id->string <method>))
+                (lambda (<param> ...) <body>)]
+               ...
+               [(equal? msg (id->string _attributes))
+                (sort (list (list (id->string <attr>) <attr>) ...) #:key car string<?)]
+               [(equal? msg (id->string _methods))
+                (sort (list (list (id->string <method>) 
+                            (lambda (<param> ...) <body>)) ...) #:key car string<?)]
+               [else "Unrecognized message!"]))
+    )]))
+
+
+; QUESTION 2 (traits).
+(define-syntax class-trait
+  (syntax-rules ()
+    
+    ))
+
+; -----------------------------------------------------------------------------
+; Class macro. This section is just for your reference.
+; -----------------------------------------------------------------------------
+(define-syntax class
+  (syntax-rules ()
+    [(class <Class> (<attr> ...)
+       [(<method> <param> ...) <body>] ...)
+     (define (<Class> <attr> ...)
+       (lambda (msg)
+         (cond [(equal? msg (id->string <attr>)) <attr>]
+               ...
+               [(equal? msg (id->string <method>))
+                (lambda (<param> ...) <body>)]
+               ...
+               [else "Unrecognized message!"]))
+       )]))
+
+(define-syntax id->string
+  (syntax-rules ()
+    [(id->string <id>)
+     (symbol->string (quote <id>))]))
