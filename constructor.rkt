145b6db8a10e2cbe23ffdf9280bd192615d28e84
fsam95 <f.samir1995@gmail.com>
2016-11-08 09:05:41 -0500

    part 1 q3 complete, just need to clean up filew

--- constructor.rkt
+++ constructor.rkt
@@ -1,10 +1,4 @@
 #lang racket
-
-#|
-User enters a list of lambdas;
-one for each attribute.
-
-|#
 (define-syntax class-with-constructor
   (syntax-rules ()
     [(class-with-constructor <Class> (<constructor-lambda> ...) (<attr> ...)
@@ -18,31 +12,52 @@ one for each attribute.
                ...
                [else "Unrecognized message!"]))
        )]))
-#|
-Can this be used to write 
 
-class MyClass:
-  def __init__(self, a, b):
-    r = f(a)
-    self.x = f(a)
-    self.y = [b, 100, r]
-    self.z = 'you are cool'
-
-def f(r):
-  return r + 5
-
-gonna need three lambdas
-
-(define (f r)
-  (+ r 5))
-(class-with-constructor MyClass ((lambda(a)(f a)) (lambda(y)(list b 100 (f a))) 
-
-
-|#
-(define (f r)
-  (+ r 5))
 (class-with-constructor MyClass ((lambda(attrs)(f (list-ref attrs 0))) (lambda(attrs)(list (list-ref attrs 1) 100 (f (list-ref attrs 0)))) (lambda(attrs)(substring "you are cool" 0))) (x y z))
 
+(define (f r)
+  (+ r 5))
+
+(class-with-constructor Point-plus-one ((lambda(attrs)(+ (list-ref attrs 0) 1)) (lambda(attrs)(+ (list-ref attrs 1) 1))) (x y))
+
+;works fine
+(define p1 (Point-plus-one 1 2))
+;(p1 "x")
+;(p1 "y")
+
+(define m1 (MyClass 1 "a" (void)))
+;(m1 "x")
+;(m1 "y")
+;(m1 "z")
+#|
+Would be better if we had 
+(class-with-constructor MyClass ((lambda(attrs)(f x)) (lambda(attrs)(y 100 (f x))) (lambda(attrs)("some const"))))
+
+Would be even better if we had
+(class-with-constructor MyClass ((f x)(list y 100 z) "some const") (x y z))
+
+|#
+
+(define-syntax cwc
+  (syntax-rules ()
+    [(cwc <Class> (<val1> ...) (<attr> ...)
+          [(<method> <param> ...) <body>] ...)
+     (define (<Class> <attr> ...)
+       (lambda (msg)
+         (cond [(equal? msg (id->string <attr>)) <val1>]
+               ...
+               [(equal? msg (id->string <method>))
+                (lambda (<param> ...) <body>)]
+               ...
+               [else "Unrecognized message!"]))
+       )]))
+
+(cwc MyClass2 ((f x) (list y 100 (f x)) "some const") (x y z))
+(define m2 (MyClass2 1 "a" (void)))
+
+(m2 "x")
+(m2 "y")
+(m2 "z")
 
 (define-syntax id->string
   (syntax-rules ()
@@ -50,14 +65,3 @@ gonna need three lambdas
      (symbol->string (quote <id>))]
     ))
 
-(class-with-constructor Point-plus-one ((lambda(attrs)(+ (list-ref attrs 0) 1)) (lambda(attrs)(+ (list-ref attrs 1) 1))) (x y))
-
-;works fine
-(define p1 (Point-plus-one 1 2))
-(p1 "x")
-(p1 "y")
-
-(define m1 (MyClass 1 "a" (void)))
-(m1 "x")
-(m1 "y")
-(m1 "z")
