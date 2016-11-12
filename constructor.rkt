f247028a04714ce7a8c06e6a26e97fb1dadf1995
fsam95 <f.samir1995@gmail.com>
2016-11-11 20:26:04 -0500

    cleaned up constructor.rkt

--- constructor.rkt
+++ constructor.rkt
@@ -1,46 +1,31 @@
 #lang racket
+
+#|
+(class-with-constructor <Class> (<val1> ...) (<attr> ...)
+  [(method <param> ...) <body>] ...])
+  ...
+
+Similar to the original class macro except it takes modifiers for the 
+parameters in the form (<val1> ...). <val1> will consist of either:
+  - function(s) applied to parameter <attr1> 
+  OR
+  - a constant 
+    - if a constant is supplied, <attr1> should be (void)
+      so that to imply that <val1> will be a constant
+
+> (define (f r) (+ r 5))
+> (class-with-constructor MyClass ((f x) (list y 100 (f x)) "some const") (x y z))
+> (define m (MyClass 1 "a" (void)))
+> (m "x")
+6 
+> (m "y")
+'("a" 100 6)
+> (m "z")
+"some const"
+|#
 (define-syntax class-with-constructor
   (syntax-rules ()
-    [(class-with-constructor <Class> (<constructor-lambda> ...) (<attr> ...)
-      [(<method> <param> ...) <body>] ...)
-     (define (<Class> <attr> ...)
-       (lambda (msg)
-         (cond [(equal? msg (id->string <attr>)) (<constructor-lambda> (list <attr> ...))]
-               ...
-               [(equal? msg (id->string <method>))
-                (lambda (<param> ...) <body>)]
-               ...
-               [else "Unrecognized message!"]))
-       )]))
-
-(class-with-constructor MyClass ((lambda(attrs)(f (list-ref attrs 0))) (lambda(attrs)(list (list-ref attrs 1) 100 (f (list-ref attrs 0)))) (lambda(attrs)(substring "you are cool" 0))) (x y z))
-
-(define (f r)
-  (+ r 5))
-
-(class-with-constructor Point-plus-one ((lambda(attrs)(+ (list-ref attrs 0) 1)) (lambda(attrs)(+ (list-ref attrs 1) 1))) (x y))
-
-;works fine
-(define p1 (Point-plus-one 1 2))
-;(p1 "x")
-;(p1 "y")
-
-(define m1 (MyClass 1 "a" (void)))
-;(m1 "x")
-;(m1 "y")
-;(m1 "z")
-#|
-Would be better if we had 
-(class-with-constructor MyClass ((lambda(attrs)(f x)) (lambda(attrs)(y 100 (f x))) (lambda(attrs)("some const"))))
-
-Would be even better if we had
-(class-with-constructor MyClass ((f x)(list y 100 z) "some const") (x y z))
-
-|#
-
-(define-syntax cwc
-  (syntax-rules ()
-    [(cwc <Class> (<val1> ...) (<attr> ...)
+    [(class-with-constructor <Class> (<val1> ...) (<attr> ...)
           [(<method> <param> ...) <body>] ...)
      (define (<Class> <attr> ...)
        (lambda (msg)
@@ -51,17 +36,25 @@ Would be even better if we had
                ...
                [else "Unrecognized message!"]))
        )]))
-
-(cwc MyClass2 ((f x) (list y 100 (f x)) "some const") (x y z))
-(define m2 (MyClass2 1 "a" (void)))
-
-(m2 "x")
-(m2 "y")
-(m2 "z")
-
 (define-syntax id->string
   (syntax-rules ()
     [(id->string <id>)
      (symbol->string (quote <id>))]
     ))
+
+(define (f r)
+  (+ r 5))
+
+#| 
+MyClass implementation
+|#
+(class-with-constructor MyClass ((f x) (list y 100 (f x)) "some const") (x y z)
+     [(addntox n)(+ x n)])
+
+(define m (MyClass 1 "a" (void)))
+
+(m "x")
+(m "y")
+(m "z")
+((m "addntox") 4)
 
